d616c0ec71550b306d851d3b70597caec620d2c3
romanswi <romanswi@buffalo.edu>
2025-02-22 18:56:09 -0500

    Added code to get and decode JSON from react

--- backend/api/signup.php
+++ backend/api/signup.php
@@ -2,6 +2,13 @@
 header("Access-Control-Allow-Origin: *")
 header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE,OPTIONS")
 
+$json = file_get_contents("php://input");
+//data should have dictionary from react?
+$data = json_decode($json);
+
+
+
+
 
 
 ?>
