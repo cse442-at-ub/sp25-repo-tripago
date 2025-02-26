cfcd721729f672daacfe7fdfe1a683922805683d
ol9999 <owenstuartlee@gmail.com>
2025-02-26 14:05:06 -0500

    struggling with the sidebar\nI do not know how to get the text aligned correctly.

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
-import "../styles/Navbar.css"
+import "../styles/Sidebar.css"
 import { useNavigate } from "react-router-dom"; 
-import logo from '../assets/Tripago_VX.png'
 
 const Sidebar = () => {
   const navigate = useNavigate();
 
   return (
       <nav className="sidebar">
-        <div className="logo">
-        <button className="logo-btn" onClick={() => navigate("/")}>
-          <img src={logo} alt="Tripago Logo" />
-        </button>
-        </div>
-        <ul className="nav-links">
-          <li><button className='navbar-links' onClick={() => navigate("/login")}>Login</button></li>
-          <li><button className='navbar-links' onClick={() => navigate("/signup")}>Signup</button></li>
+        <ul className="side-links">
+          <li><button className='sidebar-links' onClick={() => navigate("/login")}>Login</button></li>
+          <li><button className='sidebar-links' onClick={() => navigate("/signup")}>Signup</button></li>
         </ul>
       </nav>
   )
