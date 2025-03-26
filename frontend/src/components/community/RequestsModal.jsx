c39fcbbd433de9118471fc6793f0d8ef3d87cf18
romanswi <romanswi@buffalo.edu>
2025-03-26 14:19:37 -0400

    added functionallity for removing a request with the delete button

--- frontend/src/components/community/RequestsModal.jsx
+++ frontend/src/components/community/RequestsModal.jsx
@@ -31,7 +31,57 @@ const RequestsModal = ({ isOpen, onClose, type, incomingRequests,setIncomingRequ
         //if we get here, it means that the users friend has been set successfully
         //delete the entry, and alert that the friend has been added
 
+        //remove entry when success!
+        setIncomingRequests(prevRequests => prevRequests.filter(req => req.name !== name));
 
+        alert(result.message);
+      } else {
+        alert(result.message);
+      }
+      
+    } catch (error){
+
+      if (error.response) {
+        console.error("Server responded with:", error.response.data);
+        console.error("Status code:", error.response.status);
+        console.error("Headers:", error.response.headers);
+     } else if (error.request) {
+       console.error("No response received. Request:", error.request);
+     } else {  
+       console.error("Error setting up the request:", error.message);
+     }
+       console.error("Original error:", error); // Log the full error for debugging.
+    }
+
+  }
+
+
+  const deleteRequest = async(name) => {
+
+    //name should have name of person, first and last
+
+    const namesArray = name.split(" ");
+    const first_name = namesArray[0];
+    const last_name = namesArray[1];
+
+    //associative array to send to backend with first and last name
+    const formData = {first_name:first_name,last_name:last_name};
+
+    try {
+      //send request to approve request with first and last name
+      const response = await axios.post("/CSE442/2025-Spring/cse-442aj/romanTest/backend/api/deleteFrndReqst.php",formData,{
+        headers:{
+          'Content-Type':'application/json'
+        }
+      })
+
+      const result = response.data;
+
+      if (result.success){
+        //if we get here, it means that the users friend has been set successfully
+        //delete the entry, and alert that the friend has been added
+
+        //remove entry when success!
         setIncomingRequests(prevRequests => prevRequests.filter(req => req.name !== name));
 
         alert(result.message);
@@ -69,7 +119,7 @@ const RequestsModal = ({ isOpen, onClose, type, incomingRequests,setIncomingRequ
                 {req.name}
                 <div>
                   <button className="approve-btn"onClick={() => approveRequest(req.name)}>Approve</button>
-                  <button className="reset-btn">Delete</button>
+                  <button className="reset-btn" onClick={() => deleteRequest(req.name)}>Delete</button>
                 </div>
               </li>
             ))}
