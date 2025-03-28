59af3bb316b3f0d9bd40851867be5004c53592a8
Sam Jandris <samjandris@gmail.com>
2025-03-28 10:59:51 -0400

    add api keys

--- backend/config/amadeus_config.php
+++ backend/config/amadeus_config.php
@@ -1,7 +1,7 @@
 <?php
 // Amadeus API Configuration
-define('AMADEUS_API_KEY', 'AMADEUS_API_KEY');
-define('AMADEUS_API_SECRET', 'AMADEUS_API_SECRET');
+define('AMADEUS_API_KEY', '1b3RI8OUnD5tasczRvJaCoKB9Fu83Dmu');
+define('AMADEUS_API_SECRET', 'nmg2de8o96HJRDs5');
 define('AMADEUS_BASE_URL', 'https://test.api.amadeus.com'); // Use 'https://api.amadeus.com' for production
 define('AMADEUS_V1_BASE_URL', AMADEUS_BASE_URL . '/v1');
 define('AMADEUS_V2_BASE_URL', AMADEUS_BASE_URL . '/v2');
