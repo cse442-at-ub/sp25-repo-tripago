596eee769a8c76ed020ab9a6d1122f45fd41c71c
Nick Pula <nickpula@yahoo.com>
2025-04-02 18:36:12 -0400

    Updated targets

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,25 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CSE442/2025-Spring/cse-442aj/npulaTest/',
-  // base: '/CSE442/2025-Spring/cse-442aj/frontend/',
+  base: '/CSE442/2025-Spring/cse-442aj/frontend/',
   server: {
-
-      
-  base: 'http://localhost/',
-  server: {
-  proxy: {
-      "/api": {
-        target: "http://localhost", // Change to your PHP backend URL
-        changeOrigin: true,
-        secure: false, // Only needed if using HTTPS
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    }, 
-  //},
-  /*
     proxy: {
-
       '/api': {
         target: 'https://aptitude.cse.buffalo.edu/CSE442/2025-Spring/cse-442aj/backend/', // PHP server address
         changeOrigin: true,
@@ -30,7 +14,7 @@ export default defineConfig({
             proxyReq.setHeader('Host', 'https://aptitude.cse.buffalo.edu/CSE442/2025-Spring/cse-442aj/backend/');
           });
         },
-      }, */
+      }, 
     }, 
   },
 });
