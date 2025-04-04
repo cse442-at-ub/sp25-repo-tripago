287cec74cd5fb1454fb9d1f881f1d33c16cd1576
tuyisabeangelique <124276868+tuyisabeangelique@users.noreply.github.com>
2025-04-04 12:41:29 -0400

    Update path for compatibility with cattle frontend/ folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/CSE442/2025-Spring/cse-442aj/frontend2/',
+  base: '/CSE442/2025-Spring/cse-442aj/frontend/',
   server: {
     proxy: {
       '/api': {
