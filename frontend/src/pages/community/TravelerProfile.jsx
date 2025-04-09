b5bd4ee59b881e7dd00a650049a1a9a6da36fb6d
tuyisabeangelique <124276868+tuyisabeangelique@users.noreply.github.com>
2025-04-09 15:33:22 -0400

    Merge dev into 159- branch

--- frontend/src/pages/community/TravelerProfile.jsx
+++ frontend/src/pages/community/TravelerProfile.jsx
@@ -113,7 +113,7 @@ const TravelerProfile = () => {
     const fetchFriends = async () => {
       try {
         const res = await fetch(
-          "/CSE442/2025-Spring/cse-442aj/angeliqueBackend/api/getFriends.php",
+          "/CSE442/2025-Spring/cse-442aj/backend/api/getFriends.php",
           { credentials: "include" }
         );
         const data = await res.json();
