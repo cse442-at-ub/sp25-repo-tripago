586967463e09326cf37bb1ee16430af1bdecf5b0
ol9999 <owenstuartlee@gmail.com>
2025-04-23 13:52:27 -0400

    notification getter done

--- backend/api/getNotifications.php
+++ backend/api/getNotifications.php
@@ -3,6 +3,9 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: PUT,GET,POST,DELETE,OPTIONS");
 header("Content-Type: application/json");
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 // Get email from auth token
 $token = $_COOKIE['authCookie'];
 $mysqli = new mysqli("localhost","romanswi","50456839","cse442_2025_spring_team_aj_db");
@@ -19,16 +22,17 @@ if (!$email) {
 
 $stmt = $mysqli->prepare("SELECT * FROM notifications WHERE email=? ORDER BY id DESC");
 $stmt->bind_param("s", $email);
-$stmt->exectue();
-echo json_encode(["success" => true, "notifications" => $notifications, "email" => $email,]);
-exit();
+$stmt->execute();
 $result = $stmt->get_result();
 
 $notifications = [];
 
 while ($row = $result->fetch_assoc()){
-    $notifications[] = ["notification_text" => $row["notification_text"]];
+    // $notifications[] = ["notification_text" => $row["notification_text"]];
+    $notifications[] = $row["notification_text"];
 }
 
+echo json_encode(["success" => true, "notifications" => $notifications]);
+exit();
 
 ?>
